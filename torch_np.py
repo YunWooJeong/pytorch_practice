e3c75125831f0b0e68df8254ec1e467658c18deb
YunWooJeong <jeongyw12382@postech.ac.kr>
2019-03-15 22:53:10 +0900

    Git using Pycharm

--- torch_np.py
+++ torch_np.py
@@ -1,2 +1,17 @@
+
+import numpy as np
 import torch
-import numpy
+
+a = np.ones(5)
+b = torch.from_numpy(a)
+np.add(a, 1, out = a)
+print(a)
+print(b)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    y = torch.ones_like(x, device = device)
+    x = x.to(device)
+    z = x + y
+    print(z)
+    print(z.to("cpu", torch.double))
