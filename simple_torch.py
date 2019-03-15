9a8d6ee98e43e2a93d744580edfce92dc9412cdd
YunWooJeong <jeongyw12382@postech.ac.kr>
2019-03-15 22:14:56 +0900

    second_phase

--- simple_torch.py
+++ simple_torch.py
@@ -1,3 +1,25 @@
+from __future__ import print_function
 import torch
 
-a = 1
+x = torch.empty(5, 3)
+print (x)
+
+x = torch.random(5, 3)
+print (x)
+
+x = torch.zeros(5, 3, dtype = torch.long)
+print (x)
+
+x = torch.tensor([5.5, 3])
+print (x)
+
+x = x.new_ones(5, 3, dtype = torch.double)
+print (x)
+x = torch.randn_like(x, dtype = torch.float)
+print (x)
+
+print (x.size())
+
+y = torch.rand(5, 3)
+print (x + y)
+print (torch.add(x,y))
